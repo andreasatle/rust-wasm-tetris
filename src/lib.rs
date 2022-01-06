a9cec84529cc67e5c6b3ecde54c8d06f4979a060
Andreas Atle <atle.andreas@gmail.com>
2022-01-06 15:20:45 -0600

    Added wee_alloc as global allocator.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 use wasm_bindgen::prelude::*;
 
+extern crate wee_alloc;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
 pub fn greet() -> String {
     "Hello, from Rust!".to_string()
